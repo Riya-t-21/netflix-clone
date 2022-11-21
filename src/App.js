ce7bbcac1afce00b83acb35112f53d49f31c92b9
Riya-t-21 <riya0001tulswani@gmail.com>
2022-11-21 16:47:22 +0530

    InitialCommit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';      //importing css file in javascript
+import Nav from './components/nav/Nav';
+import Header from './components/header/Header';
+
+const App =   ()=>  {
+
+  return (
+  <div>
+    <Nav/>
+    <Header/>
+  </div>
+  );
+};
+
+export default App;
