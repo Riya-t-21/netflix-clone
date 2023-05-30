64637e6f8c66def03905b521f7d3e92f5db51fcb
Riya-t-21 <102308368+Riya-t-21@users.noreply.github.com>
2023-05-31 00:55:40 +0530

    Fetched Cars From API

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,7 +1,35 @@
-import './Nav.css'
+import './Nav.css';
+import { useState , useEffect } from 'react';
 const Nav = ()=>
 {
-    return <nav>
+   const [show, setShow ] = useState(false);
+
+    const scrollHandler =()=>
+    {
+        if(window.scrollY > 10)
+        {
+            setShow(true);
+        }
+        else{
+            setShow(false);
+        }
+    };
+
+   useEffect(()=>
+   {
+     window.addEventListener('scroll', scrollHandler);
+       
+
+      return ()=>{
+        window.removeEventListener('scroll',scrollHandler);
+      };
+
+   }, []);
+
+
+    return <nav style={{
+        backgroundColor:  show ? 'rgb(20,20,20)': ' transparent',
+    }}>
        
        <section> 
         
