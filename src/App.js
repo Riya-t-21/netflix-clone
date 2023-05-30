b07d5c6f168d2440e6bda6ec4853245a7baa80b7
Riya-t-21 <102308368+Riya-t-21@users.noreply.github.com>
2023-05-31 00:53:37 +0530

    Fetched Cards From API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';      //importing css file in javascript
 import Nav from './components/nav/Nav';
 import Header from './components/header/Header';
+import Content from './components/content/Content';
+import axios from 'axios';
+
+
+axios.defaults.baseURL='https://api.themoviedb.org/3';
+axios.defaults.params ={
+  api_key: 'c5a106392e39f6bbd2abd14dd133cbc4',
+};
+
+
+
 
 const App =   ()=>  {
 
@@ -8,6 +19,7 @@ const App =   ()=>  {
   <div>
     <Nav/>
     <Header/>
+    <Content/>
   </div>
   );
 };
